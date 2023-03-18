d9ea8f518b305b815e5efabd4d69a25a84bc63ec
Jail5on <jss91@outlook.com>
2023-03-17 21:38:47 -0300

    Novo projeto inicial de aprendizagem

---
+++ ConsoleAppNet/Program.cs
@@ -0,0 +1,7 @@
+﻿
+Console.WriteLine("=============\r\nPar ou Ímpar?\r\n=============");
+Console.Write("Informe um número qualquer: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+string parImpar = numero % 2 == 0 ? "Par" : "Ímpar"; // operador ternário
+Console.WriteLine($"O número {numero} é {parImpar}");
